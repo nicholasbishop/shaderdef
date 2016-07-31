e0feadce045d307ec289e7221b0fed8c4f8ca029
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-31 02:41:31 -0400

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shaderdef',
+      version='0.5.0',
+      description='The funniest joke in the world',
+      url='http://github.com/nicholasbishop/shaderdef',
+      author='Nicholas Bishop',
+      author_email='nicholasbishop@gmail.com',
+      license='GNU General Public License v3 or later (GPLv3+)',
+      packages=['shaderdef'],
+      zip_safe=True)
