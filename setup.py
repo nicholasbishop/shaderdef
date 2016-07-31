d3fe3bcb31689b13a8d6902ce13ebba539f7408e
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-31 02:41:48 -0400

    https

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='shaderdef',
       version='0.5.0',
       description='The funniest joke in the world',
-      url='http://github.com/nicholasbishop/shaderdef',
+      url='https://github.com/nicholasbishop/shaderdef',
       author='Nicholas Bishop',
       author_email='nicholasbishop@gmail.com',
       license='GNU General Public License v3 or later (GPLv3+)',
