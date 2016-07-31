d31273e72d5180bde73467019a3e7959e43de6fa
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-30 23:20:02 -0400

    Add equality mixin
    Source: http://stackoverflow.com/questions/390250/elegant-ways-to-support-equivalence-equality-in-python-classes

---
+++ shaderdef/equality.py
@@ -0,0 +1,8 @@
+# Source: http://stackoverflow.com/questions/390250/elegant-ways-to-support-equivalence-equality-in-python-classes
+class EqualityMixin(object):
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
