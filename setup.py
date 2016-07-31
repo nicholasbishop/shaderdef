748119ddff4e0784983a5a82e6aa85d81a01d062
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-31 02:53:42 -0400

    Make "setup.py test" work

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup
 
 setup(name='shaderdef',
-      version='0.5.1',
+      version='0.5.2',
       description='Transform Python code into GLSL shaders',
       url='https://github.com/nicholasbishop/shaderdef',
       author='Nicholas Bishop',
       author_email='nicholasbishop@gmail.com',
       license='GNU General Public License v3 or later (GPLv3+)',
       packages=['shaderdef'],
+      test_suite='test',
       zip_safe=True)
