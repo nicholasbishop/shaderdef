b6ceff85340b040dd04d1b65ccd27cdca17ad73c
Nicholas Bishop <nicholasbishop@gmail.com>
2016-08-06 18:14:32 -0400

    Rename assertCodeEqual -> assert_code_equal

--- test/test_code_gen.py
+++ test/test_code_gen.py
@@ -5,31 +5,31 @@ from shaderdef.py_to_glsl import py_to_glsl
 from test.util import deindent
 
 class TestPyToGlsl(TestCase):
-    def assertCodeEqual(self, pycode, glslcode):
+    def assert_code_equal(self, pycode, glslcode):
         root = ast.parse(pycode)
         code = '\n'.join(py_to_glsl(root))
         self.assertEqual(deindent(code), glslcode)
 
     def test_empty_function(self):
-        self.assertCodeEqual('def myFunc(): pass', 'void myFunc() {}')
+        self.assert_code_equal('def myFunc(): pass', 'void myFunc() {}')
 
     def test_assign(self):
-        self.assertCodeEqual('a = b', 'a = b')
+        self.assert_code_equal('a = b', 'a = b')
 
     def test_binop(self):
-        self.assertCodeEqual('a - b * c / d', '(a - ((b * c) / d))')
+        self.assert_code_equal('a - b * c / d', '(a - ((b * c) / d))')
 
     def test_subscript(self):
-        self.assertCodeEqual('a[0]', 'a[0]')
+        self.assert_code_equal('a[0]', 'a[0]')
 
     def test_unaryop(self):
-        self.assertCodeEqual('-a', '-a')
+        self.assert_code_equal('-a', '-a')
 
     def test_augassign(self):
-        self.assertCodeEqual('a += 1', 'a += 1')
+        self.assert_code_equal('a += 1', 'a += 1')
 
     def test_compare(self):
-        self.assertCodeEqual('a < b', 'a < b')
+        self.assert_code_equal('a < b', 'a < b')
 
     def test_if(self):
-        self.assertCodeEqual('if a: a = b', 'if (a) {a = b;}')
+        self.assert_code_equal('if a: a = b', 'if (a) {a = b;}')
