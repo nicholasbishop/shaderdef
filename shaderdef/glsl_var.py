722bfe87ea3887d9566c97c8a404b72d84d1b612
Nicholas Bishop <nicholasbishop@gmail.com>
2016-08-15 19:49:43 -0400

    Test fixes

--- shaderdef/glsl_var.py
+++ shaderdef/glsl_var.py
@@ -1,18 +1,28 @@
 class GlslVar(object):
     """Represent a GLSL variable declaration (or struct member)."""
-    def __init__(self, name, gtype):
+    def __init__(self, name, gtype, interpolation=None):
         self.name = name
         self.gtype = gtype
+        self.interpolation = interpolation
+
+    def _gdecl(self, *parts):
+        not_none = (part for part in parts if part is not None)
+        return '{};'.format(' '.join(not_none))
 
     def declare(self):
-        return '{} {};'.format(self.gtype, self.name)
+        return self._gdecl(self.interpolation, self.gtype, self.name)
 
     def declare_uniform(self):
-        return 'uniform {}'.format(self.declare())
+        return self._gdecl('uniform', self.gtype, self.name)
 
     def declare_attribute(self, location=None):
         if location is None:
-            location_str = ''
+            location_str = None
         else:
             location_str = 'layout(location={}) '.format(int(location))
-        return '{}in {} {};'.format(location_str, self.gtype, self.name)
+
+        return self._gdecl(location_str, self.interpolation, 'in',
+                           self.gtype, self.name)
+
+    def declare_output(self):
+        return self._gdecl(self.interpolation, 'out', self.gtype, self.name)
--- test/test_glsl_var.py
+++ test/test_glsl_var.py
@@ -4,12 +4,21 @@ from shaderdef.glsl_var import GlslVar
 
 class TestGlslVar(TestCase):
     def test_declare(self):
-        self.assertEqual(GlslVar('x', 'vec3').declare(), 'vec3 x;')
+        var = GlslVar('x', 'vec3')
+        self.assertEqual(var.declare(), 'vec3 x;')
 
     def test_declare_uniform(self):
-        self.assertEqual(GlslVar('x', 'vec3').declare_uniform(),
-                         'uniform vec3 x;')
+        var = GlslVar('x', 'vec3')
+        self.assertEqual(var.declare_uniform(), 'uniform vec3 x;')
 
     def test_declare_attribute(self):
-        self.assertEqual(GlslVar('x', 'vec3').declare_attributes(),
-                         'in vec3 x;')
+        var = GlslVar('x', 'vec3')
+        self.assertEqual(var.declare_attribute(), 'in vec3 x;')
+
+    def test_declare_output(self):
+        var = GlslVar('x', 'vec3')
+        self.assertEqual(var.declare_output(), 'out vec3 x;')
+
+    def test_declare_interpolation(self):
+        var = GlslVar('x', 'vec3', interpolation='noperspective')
+        self.assertEqual(var.declare_attribute(), 'noperspective in vec3 x;')
