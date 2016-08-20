b48174d018454d7beb1116b6fe66d00dbdbec755
Nicholas Bishop <nicholasbishop@gmail.com>
2016-08-20 16:04:27 -0400

    Add repr/hash/eq to GlslVar

--- shaderdef/glsl_var.py
+++ shaderdef/glsl_var.py
@@ -1,3 +1,5 @@
+from shaderdef.equality import EqualityMixin
+
 def _gdecl(*parts):
     not_none = (part for part in parts if part is not None)
     return '{};'.format(' '.join(not_none))
@@ -10,12 +12,21 @@ def location_str(location):
         return 'layout(location={})'.format(int(location))
 
 
-class GlslVar(object):
+class GlslVar(EqualityMixin):
     """Represent a GLSL variable declaration (or struct member)."""
     def __init__(self, name, gtype, interpolation=None):
         self.name = name
         self.gtype = gtype
         self.interpolation = interpolation
+
+    def __hash__(self):
+        return hash((self.name, self.gtype, self.interpolation))
+
+    def __repr__(self):
+        parts = [self.name, self.gtype]
+        if self.interpolation is not None:
+            parts.append(self.interpolation)
+        return 'GlslVar({})'.format(' '.join(parts))
 
     def declare(self):
         return _gdecl(self.interpolation, self.gtype, self.name)
