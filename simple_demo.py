93ee46d3ed70b842a67112b57b6cb5005b0cd20d
Nicholas Bishop <nicholasbishop@gmail.com>
2016-08-29 02:17:00 -0400

    Expand docstring

--- simple_demo.py
+++ simple_demo.py
@@ -1,6 +1,21 @@
 #! /usr/bin/env python3
 
-"""
+"""This demo shows how to define a very simple shader program with a
+vertex shader and a fragment shader.
+
+The vertex shader is defined in the `vert_shader` function. Note that
+the inputs and outputs are annotated; this is how `shaderdef` knows
+what types to use in the generated GLSL code.
+
+Inputs and outputs are grouped together using a Python class. For
+example, the shader program's vertex attributes are defined in
+`VsIn`. It inherits from `AttributeBlock` to mark its members as vertex
+attributes. (There's a UniformBlock for declaring uniform inputs.)
+
+Shader outputs are set using the `return` keyword. The return type
+should be a class such as `VsOut`; pass the outputs as keyword
+arguments.
+
 >>> sdef = ShaderDef(vert_shader=vert_shader, frag_shader=frag_shader)
 >>> sdef.translate()
 
@@ -17,6 +32,7 @@ layout(location=0) out vec4 color;
 void main() {
     color = vec4(1.0, 0.0, 0.0, 1.0);
 }
+
 """
 
 from shaderdef import (AttributeBlock, FragmentShaderOutputBlock,
