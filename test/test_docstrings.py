179c3b7240ffdf6074b2cbbaccda78cca4f1d703
Nicholas Bishop <nicholasbishop@gmail.com>
2016-08-29 02:16:47 -0400

    Lint fix

--- test/test_docstrings.py
+++ test/test_docstrings.py
@@ -1,7 +1,7 @@
-import unittest
 import doctest
 import simple_demo
 
+# pylint: disable=unused-argument
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(simple_demo))
     return tests
