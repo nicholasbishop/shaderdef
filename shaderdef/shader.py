beeaf1aa1e26880f893d42ca43b25113ff52e3b9
Nicholas Bishop <nicholasbishop@gmail.com>
2016-08-29 00:08:21 -0400

    Make geometry shader optional

--- shaderdef/shader.py
+++ shaderdef/shader.py
@@ -4,11 +4,14 @@ from shaderdef.stage import Stage
 
 @attr.s(init=False)
 class ShaderDef(object):
-    def __init__(self, vert_shader, geom_shader, frag_shader):
-        self._vert_shader = Stage(vert_shader)
-        self._geom_shader = Stage(geom_shader)
-        self._frag_shader = Stage(frag_shader)
+    def __init__(self, vert_shader, frag_shader, geom_shader=None):
         self._library = []
+        self._vert_shader = Stage(vert_shader)
+        self._frag_shader = Stage(frag_shader)
+        if geom_shader is None:
+            self._geom_shader = None
+        else:
+            self._geom_shader = Stage(geom_shader)
 
     def add_function(self, function):
         """Add a utility `function` to the shader program.
@@ -21,8 +24,9 @@ class ShaderDef(object):
 
     def translate(self):
         self._vert_shader.translate(self._library)
-        self._geom_shader.translate(self._library)
         self._frag_shader.translate(self._library)
+        if self._geom_shader is not None:
+            self._geom_shader.translate(self._library)
 
     @property
     def vert_shader(self):
@@ -32,6 +36,8 @@ class ShaderDef(object):
     @property
     def geom_shader(self):
         """Get the GLSL code for the geometry shader."""
+        if self._geom_shader is None:
+            raise ValueError('this shaderdef does not have a geometry shader')
         return self._geom_shader.glsl
 
     @property
